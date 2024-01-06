d78d607aad45cdd1d074a3a1f0fe9bdd1380194b
tanvirislamrakib <155340897+tanvirislamrakib@users.noreply.github.com>
2024-01-06 16:48:29 +0600

    Add files via upload

---
+++ else.js
@@ -0,0 +1,6 @@
+if(10 < 1){
+    console.log("Hello World")
+}
+else{
+    console.log("Hacker World")
+}
---
+++ if.js
@@ -0,0 +1,12 @@
+//if statement
+var a = 2;
+var b = 5;
+if(a < b){
+    console.log("Computer er matha this ase")
+}
+
+
+var a = 2;
+if(a > 10){
+    console.log("Computer gaja khayse")
+}
